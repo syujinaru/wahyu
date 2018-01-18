0e0ea06c8aee0ba31517b1caeaaf7bcc4921a252
syujinaru <wahyu007.ganz@gmail.com>
2018-01-18 13:40:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ restService.post("/audio", function(req, res) {
     //Speech Synthesis Markup Language 
     case "qolam":
       speech =
-        '<speak><audio src="http://wahyutrilaksono.business/001_Al_Faatihah.ogg">did not get your audio file</audio>Assalamualaikum! Welcome to Al-Qolam!, Allows users to learn, read and listen the Quran in a fast, simple, accurate and very accessible way by using google assistant</speak>';
-        '<speak>Would you like to Read and Listen Quran.?</speak>';
+       '<speak>Here are <say-as interpret-as="characters">SSML</say-as> samples. I can pause <break time="3s"/>.I can play a sound<audio src="https://www.example.com/MY_MP3_FILE.mp3">did not get your MP3 audio file</audio>.I can speak in cardinals. Your number is <say-as interpret-as="cardinal">10</say-as>.Or I can speak in ordinals. You are <say-as interpret-as="ordinal">10</say-as> in line. Or I can even speak in digits. The digits for ten are <say-as interpret-as="characters">10</say-as>.I can also substitute phrases, like the <sub alias="World Wide Web Consortium">W3C</sub>.Finally, I can speak a paragraph with two sentences.<p><s>This is sentence one.</s><s>This is sentence two.</s></p></speak>';
       break;
     case "music two":
       speech =
