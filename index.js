553f66f64264b3cc0c1c5c65f8e6ea252b1268f6
syujinaru <wahyu007.ganz@gmail.com>
2018-01-18 13:05:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ restService.post("/audio", function(req, res) {
     //Speech Synthesis Markup Language 
     case "qolam":
       speech =
-        '<speak><audio src="http://wahyutrilaksono.business/001_Al_Faatihah.ogg">did not get your audio file</audio>Ok</speak>';
+        '<speak><audio src="http://wahyutrilaksono.business/001_Al_Faatihah.ogg">did not get your audio file</audio>Assalamualaikum! Welcome to Al-Qolam!</speak>';
       break;
     case "music two":
       speech =
