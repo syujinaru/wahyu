3b77df7ae28f47af978ad449ae7fbd46aa8a13b0
syujinaru <wahyu007.ganz@gmail.com>
2018-01-25 11:39:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ restService.post("/audio", function(req, res) {
   var speech = "";
   switch (req.body.result.parameters.AudioSample.toLowerCase()) {
     //Speech Synthesis Markup Language 
-    case "Talk to Al-Qolam!":
+    case "qolam":
       speech =
           '<speak> Assalamualaikum! Selamat datang di Al-Qolam! <break time="3s"/> <audio src="https://klinikkita.net/001_Al_Faatihah.ogg">tidak bisa mengkoneksikan audio</audio> Kami siap menemani Anda untuk belajar, membaca dan mendengarkan Al-Qur’an. Apa yang ingin Anda baca dan dengarkan?</speak>';
      break;
