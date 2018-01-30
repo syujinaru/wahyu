f03ffcbfd1dac09402eaf5703a1f66e6a59525a0
syujinaru <wahyu007.ganz@gmail.com>
2018-01-31 01:20:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,13 +46,11 @@ restService.post("/echo", function list () {
 
 restService.post("/audio", function(req, res) {
   var speech = "";
-  const app = new App({request, response});
   switch (req.body.result.parameters.AudioSample.toLowerCase()) {
     //Speech Synthesis Markup Language 
     case "qolam":
       speech =
           '<speak> Assalamualaikum! Selamat datang di Al-Qolam! <break time="3s"/> <audio src="https://klinikkita.net/001_Al_Faatihah.ogg">tidak bisa mengkoneksikan audio</audio> Kami siap menemani Anda untuk belajar, membaca dan mendengarkan Al-Qur’an. Apa yang ingin Anda baca dan dengarkan? [Murottal Al-Quran] [Murottal dan Terjemahan] [Do’a-do’a] [Ayat-Ayat Tematik] [Tafsir Al-Quran]</speak>';
-        app.ask('Murottal Al-Quran');
       break;
     case "Murottal Al-Quran":
       speech =
