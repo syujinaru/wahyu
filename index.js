a69e9cba9688dbf46c8f6e686f3034edaf0eaf14
syujinaru <wahyu007.ganz@gmail.com>
2018-01-31 08:56:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -45,14 +45,14 @@ restService.post("/audio", function(req, res) {
      break;
     case "Abdurrahman As Sudays":
       speech =
-       '<speak>Selamat mendengarkan qori pilihan Anda, Untuk pilihan lebih lengkap silakan download Aplikasi Al-Qolam!<a href='https://play.google.com/store/apps/details?id=com.exceptionaire.alqolam'> <img src="http://alqolam.com/wp-content/uploads/2015/04/alqolamlogo.png">not connected image</img></a><break time="3s"/> <audio src="https://klinikkita.net/001_Al_Faatihah.ogg">tidak bisa mengkoneksikan audio</audio> Apakah Anda mau mendengarkan surah favorit Anda yang lain?[Surah Alfatihah] [Surah Yasin] [Surah Al Waqiah] [Surah Al-Mulk] [Surah Ar-Rahman] [Al-Maidah] [Tidak mau]</speak>';
+       '<speak>Selamat mendengarkan qori pilihan Anda, Untuk pilihan lebih lengkap silakan download Aplikasi Al-Qolam! <img src="http://alqolam.com/wp-content/uploads/2015/04/alqolamlogo.png">not connected image</img><break time="3s"/> <audio src="https://klinikkita.net/001_Al_Faatihah.ogg">tidak bisa mengkoneksikan audio</audio> Apakah Anda mau mendengarkan surah favorit Anda yang lain?[Surah Alfatihah] [Surah Yasin] [Surah Al Waqiah] [Surah Al-Mulk] [Surah Ar-Rahman] [Al-Maidah] [Tidak mau]</speak>';
       break;
     //Bad Input
     case "Lagu Raisha":
       speech = 
 	     '<speak>Sepertinya bukan itu yang kami maksud</speak>';
     break;
-    case "Tidak mau2":
+    case "Tidak Good":
       speech =
         '<speak>Apakah Anda suka surah Yasin?</speak>';
       break;
@@ -60,51 +60,11 @@ restService.post("/audio", function(req, res) {
       speech =
          '<speak> Baik. Selamat mendengarkan surah Yasin dari Qori Mishary Ibnu Rashid Al Afasy!<break time="3s"/> <audio src="https://klinikkita.net/001_Al_Faatihah.ogg">tidak bisa mengkoneksikan audio</audio> Apakah Anda mau mendengarkan surah favorit Anda yang lain? [Surah Alfatihah] [Surah Yasin] [Surah Al Waqiah] [Surah Al-Mulk] [Surah Ar-Rahman] [Al-Maidah] [Tidak mau] </speak>';
       break;
-    case "Tidak Mau3":
+    case "Tidak Ok":
       speech =
-        '<speak>Baiklah. Terima kasih. Sampai ketemu lagi. Untuk lebih lengkap silakan download Aplikasi Al-Qolam! <a href='https://play.google.com/store/apps/details?id=com.exceptionaire.alqolam'> <img src="http://alqolam.com/wp-content/uploads/2015/04/alqolamlogo.png">not connected image</img></a></speak>';
+        '<speak>Baiklah. Terima kasih. Sampai ketemu lagi. Untuk lebih lengkap silakan download Aplikasi Al-Qolam! <img src="http://alqolam.com/wp-content/uploads/2015/04/alqolamlogo.png">not connected image</img></a></speak>';
       break;
-    case "bleep":
-      speech =
-        '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
-      break;
-    case "unit":
-      speech =
-        '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
-      break;
-    case "verbatim":
-      speech =
-        '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
-      break;
-    case "date one":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
-      break;
-    case "date two":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
-      break;
-    case "date three":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
-      break;
-    case "time":
-      speech =
-        '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
-      break;
-    case "telephone one":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
-      break;
-    case "telephone two":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
-      break;
-    // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
-    case "alternate":
-      speech =
-        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
-      break;
+  
   }
   return res.json({
     speech: speech,
